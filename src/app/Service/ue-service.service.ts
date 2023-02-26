154f63b7a97b1143d973a36bf8f1e2762e4757f5
NaderBessioud <79223546+NaderBessioud@users.noreply.github.com>
2023-02-26 12:13:45 +0100

    Update ue-service.service.ts

--- src/app/Service/ue-service.service.ts
+++ src/app/Service/ue-service.service.ts
@@ -8,7 +8,7 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class UeServiceService {
-  url : string = 'http://127.0.0.1:8082/EspritHub/';
+  url : string = 'http://192.168.107.228:8082/EspritHub/';
 
   constructor(private http:HttpClient) { }
 
