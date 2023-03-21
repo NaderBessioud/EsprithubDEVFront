01cebc974103ff029ff04fcd84da962a265ef779
NaderBessioud <79223546+NaderBessioud@users.noreply.github.com>
2023-03-21 10:02:53 +0100

    Update lien-utile.service.ts

--- src/app/Service/lien-utile.service.ts
+++ src/app/Service/lien-utile.service.ts
@@ -5,7 +5,7 @@ import {lienUtile} from '../Entities/lienUtile'
   providedIn: 'root'
 })
 export class LienUtileService {
-  url : string = 'http://192.168.1.20:8082/EspritHub/';
+  url : string = 'http://192.168.2.179:8082/EspritHub/';
 
   constructor(private http:HttpClient) { }
 
