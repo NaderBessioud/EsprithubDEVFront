f339b1b36d74932fa57e71049ac3ada8640bfa5e
NaderBessioud <79223546+NaderBessioud@users.noreply.github.com>
2023-03-25 18:10:41 +0100

    Update tag-service.service.ts

--- src/app/Service/tag-service.service.ts
+++ src/app/Service/tag-service.service.ts
@@ -8,7 +8,7 @@ import {UserQuestion} from "../Entities/user-question";
   providedIn: 'root'
 })
 export class TagServiceService {
-  url : string = 'http://192.168.1.21:8082/EspritHub/';
+  url : string = 'http://192.168.1.17:8082/EspritHub/';
 
   constructor(private http: HttpClient) { }
 
